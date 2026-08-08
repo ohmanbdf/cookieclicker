08b92983227515445246976100f0cd7067b05fcc
hylment <rc9551@education.nsw.gov.au>
2026-08-08 17:01:10 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ let no = 1;
 let count = document.querySelector('h2');
 let img = document.querySelector('img');
 function update() {count.textContent = no;}
-function add() {no++ update()}
+function add() {no++; update()}
