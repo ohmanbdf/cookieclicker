cdbaec6ecce5aca4ecd9432e7e8895ba6105002a
hylment <312482092+ohmanbdf@users.noreply.github.com>
2026-09-23 21:35:50 +1000

    Change click limit from 1 to 99999 in add function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 let no = 0;
 let count = document.querySelector('h2');;
 function update() {count.textContent = no;}
-function add() {if (no == 1){let answer = confirm('Continue Clicking?'); if (answer == true){game.classList.add('hidden'); hi.classList.remove('hidden')} else{game.classList.add('hidden'); h.classList.remove('hidden')}}else{no++; update()}}
+function add() {if (no == 99999){let answer = confirm('Continue Clicking?'); if (answer == true){game.classList.add('hidden'); hi.classList.remove('hidden')} else{game.classList.add('hidden'); h.classList.remove('hidden')}}else{no++; update()}}
